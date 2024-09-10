8c7a9ef8e669d09db679967057b47fc07cc8b0d5
Mohamed Safras <mohamed.safras@acentura.com>
2024-09-10 15:48:57 +0530

    experience animation has been added

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,57 +1,50 @@
 "use client";
-import React from "react";
+import React, { Suspense, useState } from "react";
 
-import { workExperience } from "@/data";
-import { Button } from "./ui/MovingBorders";
-import Image from "next/image";
-import { client } from "../sanity/sanity";
-import { groq } from "next-sanity";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import Developer from "./Developer";
+import { TimelineExperience } from "./TimeLineExperience";
 
 const Experience = () => {
-  return (
-    <section className="py-20 w-full" id="experience">
-      <h1 className="heading">
-        My <span className="text-purple">work experience</span>
-      </h1>
+  const [animationName, setAnimationName] = useState<string>("victory");
 
-      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
-        {workExperience.map((card) => (
-          <Button
-            key={card.id}
-            //   random duration will be fun , I think , may be not
-            duration={Math.floor(Math.random() * 10000) + 10000}
-            borderRadius="1.75rem"
-            style={{
-              //   add these two
-              //   you can generate the color from here https://cssgradient.io/
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-              // add this border radius to make it more rounded so that the moving border is more realistic
-              borderRadius: `calc(1.75rem* 0.96)`,
-            }}
-            // remove bg-white dark:bg-slate-900
-            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-          >
-            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
-              <Image
-                src={card.thumbnail}
-                alt={card.thumbnail}
-                width={32}
-                height={32}
-                className="lg:w-32 md:w-20 w-16"
-              />
-              <div className="lg:ms-5">
-                <h1 className="text-start text-xl md:text-2xl font-bold">
-                  {card.title}
-                </h1>
-                <p className="text-start text-white-100 mt-3 font-semibold">
-                  {card.desc}
-                </p>
-              </div>
+  const handleAnimation = (animationName: string) => {
+    setAnimationName(animationName);
+  };
+
+  return (
+    <section className="py-20" id="experience">
+      <div className="w-full text-white-600">
+        <h3 className="heading ">
+          My Work <span className="text-purple">Experience</span>
+        </h3>
+
+        <div className="grid gap-y-5 md:gap-5 lg:grid-cols-3 grid-cols-1  mt-12">
+          <div className="col-span-1  rounded-lg bg-black-100 border border-black-300">
+            <Canvas>
+              <ambientLight intensity={7} />
+              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+              <directionalLight position={[10, 10, 10]} intensity={1} />
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+
+              <Suspense fallback={<CanvasLoader />}>
+                <Developer
+                  position-y={-2}
+                  scale={2}
+                  animationName={animationName}
+                />
+              </Suspense>
+            </Canvas>
+          </div>
+
+          <div className="col-span-2 rounded-lg bg-black-100 border border-black-300">
+            <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+              <TimelineExperience handleAnimation={handleAnimation} />
             </div>
-          </Button>
-        ))}
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ components/TimeLineExperience.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import { Timeline } from "./ui/Timeline";
+
+export function TimelineExperience({
+  handleAnimation,
+}: {
+  handleAnimation: (animationName: string) => void;
+}) {
+  const data = [
+    {
+      title: "2024",
+      animationName: "salute",
+      content: (
+        <div>
+          <h3 className="text-sm font-semibold mb-1 md:text-base lg:text-lg">
+            Acentura Inc
+          </h3>
+          <div className="flex flex-col md:flex-row">
+            <p className="text-xs mb-1 md:text-sm lg:text-base">
+              Software Engineer
+            </p>
+            <p className="text-xs text-neutral-500 mb-1 md:ml-2 md:text-sm lg:text-base">
+              ( 2024 - Present )
+            </p>
+          </div>
+
+          <div className=" mb-8">
+            <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal">
+              task handeled
+            </p>
+          </div>
+
+          <div className=" mb-8">special movments</div>
+        </div>
+      ),
+    },
+    {
+      title: "Early 2023",
+      animationName: "clapping",
+      content: (
+        <div>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="https://assets.aceternity.com/pro/hero-sections.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-15 md:h-20 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="https://assets.aceternity.com/features-section.png"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-15 md:h-20 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+  ];
+  return <Timeline data={data} handleAnimation={handleAnimation} />;
+}
